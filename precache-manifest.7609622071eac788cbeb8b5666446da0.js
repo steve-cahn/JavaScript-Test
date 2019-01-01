64f0be8edc039729326c9f5e73055553fd299e7e
Steve Cahn <steve@stevecahn.net>
2019-01-01 13:28:58 -0800

    Updates

---
+++ precache-manifest.7609622071eac788cbeb8b5666446da0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "53a818ba6c6fce7bfd57091b7b767d92",
+    "url": "/Javascript-test/static/media/logo_outline.53a818ba.png"
+  },
+  {
+    "revision": "90bbc4e055885967fdc86aa57ddbb2ff",
+    "url": "/Javascript-test/static/media/building.90bbc4e0.svg"
+  },
+  {
+    "revision": "ba0338bb6ded50f9cc1f",
+    "url": "/Javascript-test/static/js/runtime~main.ba0338bb.js"
+  },
+  {
+    "revision": "b0ae7536b563f9002e8c",
+    "url": "/Javascript-test/static/js/main.b0ae7536.chunk.js"
+  },
+  {
+    "revision": "8fe0b3548ac9af2fb6b9",
+    "url": "/Javascript-test/static/js/1.8fe0b354.chunk.js"
+  },
+  {
+    "revision": "b0ae7536b563f9002e8c",
+    "url": "/Javascript-test/static/css/main.e6527c6a.chunk.css"
+  },
+  {
+    "revision": "ea8d4788111d3e596d8c3fe99e90d63c",
+    "url": "/Javascript-test/index.html"
+  }
+];
